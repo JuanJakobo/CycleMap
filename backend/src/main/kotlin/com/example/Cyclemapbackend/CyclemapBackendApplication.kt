d360ca4f8350e3576ffb7aad514045263c92f828
JuanJakobo <github@johannsn.com>
2022-09-12 17:20:57 +0200

    Include SpringBoot config for kotlin backend

---
+++ backend/src/main/kotlin/com/example/Cyclemapbackend/CyclemapBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Cyclemapbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CyclemapBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CyclemapBackendApplication>(*args)
+}

fcc0267ab8f26e01b0f0c28b72a4b5cf9c82159f
JuanJakobo <github@johannsn.com>
2022-09-20 10:39:06 +0200

    Add method getCoordinates to backend

--- backend/src/main/kotlin/com/johannsn/cyclemapbackend/controller/CoordinatesController.kt
+++ backend/src/main/kotlin/com/johannsn/cyclemapbackend/controller/CoordinatesController.kt
@@ -9,8 +9,20 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
+//TODO
+@CrossOrigin("http://localhost:3001")
 class CoordinatesController(val coordinatesRepository : CoordinatesRepository, val tripRepository: TripRepository) {
 
+    //TODO only every second?
+    //get coordinates for tour and not for trip?
+    @GetMapping("/trips/{tripId}/coordinates")
+    fun getCoordinates(@PathVariable("tripId") tripId: Long): ResponseEntity<List<Coordinates>>? {
+        return if (tripRepository.existsById(tripId)) {
+            ResponseEntity(coordinatesRepository.findByTripId(tripId), HttpStatus.OK)
+        } else {
+            throw EntityNotFoundException("The Trip $tripId does not exist.")
+        }
+    }
     @PostMapping("/trips/{tripId}/coordinates")
     fun postCoordinates(@PathVariable("tripId") tripId: Long, @RequestBody coordinates: List<Coordinates>): ResponseEntity<List<Coordinates>>? {
         return tripRepository.findById(tripId).map { trip ->

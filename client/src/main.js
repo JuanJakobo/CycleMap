f8caf0bb0e83f46e050a438350749c3f30b5b1ed
JuanJakobo <github@johannsn.com>
2022-10-03 13:50:57 +0200

    update vue version

--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'leaflet/dist/leaflet.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.component('l-map', LMap);
-Vue.component('l-tile-layer', LTileLayer);
-Vue.component('l-marker', LMarker);
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
-// eslint-disable-next-line  
-delete L.Icon.Default.prototype._getIconUrl  
-// eslint-disable-next-line  
-L.Icon.Default.mergeOptions({  
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),  
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),  
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')  
+// eslint-disable-next-line
+delete L.Icon.Default.prototype._getIconUrl
+// eslint-disable-next-line
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 })
 
 new Vue({
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 module.exports = {
     devServer: {
-        port: 3001    
+        port: 3001,
     }
 }

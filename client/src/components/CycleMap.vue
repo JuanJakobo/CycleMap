f0275e2d8f69b2c47da58e5a0e3d9ee908a0daea
JuanJakobo <github@johannsn.com>
2022-10-04 18:34:20 +0200

    Put all data into one array, improve error messaging

--- client/src/components/CycleMap.vue
+++ client/src/components/CycleMap.vue
@@ -12,29 +12,30 @@
     >
       <!-- top left -->
       <l-control position="topleft">
+      <section v-show="loaded">
         <table>
           <tr>
-            <section v-show="!failedToLoad">
             <td>
               <b-form-select
                 v-model="selectedTour"
-                id="tourDropdown"
-                :options="tourDropdown"
+                :options="tours"
                 v-on:change="updateTourDropdown"
               />
             </td>
             <td>
-             <section v-show="tourHasTrips">
+      <section v-if="tourHasTrips">
               <b-form-select
                 v-model="selectedTrip"
-                :options="tripDropdown"
+                :options="tours[currentTourIndex].trips"
+                value-field="id"
+                text-field="title"
                 v-on:change="updateTripDropdown"
               />
-              </section>
-            </td>
             </section>
+            </td>
           </tr>
         </table>
+            </section>
       </l-control>
 
       <!-- bottom left -->
@@ -53,7 +54,43 @@
       ></l-control-scale>
       <l-tile-layer :url="url" :attribution="attribution" />
 
-      <!-- Polylines -->
+      <!-- Info modal -->
+        <p>
+        <b-modal
+        id="bv-modal-info"
+        hide-footer
+        scrollable
+        hide-header-close
+        title="Scrollable Content"
+        size="xl"
+        >
+        <template #modal-title>
+          Info
+        </template>
+        Version 0.1
+        <b-button class="mt-3" block @click="closeInfo()">Close</b-button >
+      </b-modal>
+        </p>
+
+      <!-- loading Modal -->
+      <b-modal
+        id="bv-modal-loading"
+        hide-header
+        hide-footer
+        scrollable
+        hide-header-close
+        title="Scrollable Content"
+        size="sm"
+      >
+      <center>
+        <b-button class="mt-3" disabled>
+        <b-spinner small type="grow"></b-spinner>
+        Loading...
+        </b-button>
+      </center>
+      </b-modal>
+
+      <!-- Error modal -->
         <p>
         <b-modal
         id="bv-modal-error"
@@ -70,20 +107,31 @@
         <b-button class="mt-3" block @click="retryConnect()">Retry</b-button >
       </b-modal>
         </p>
-      <section v-if="!failedToLoad">
-        <div v-if="tourHasTrips">
+      <!-- Polylines -->
+      <section v-if="tourHasTrips">
         <l-polyline
-          v-for="item in tripsForTour"
+          v-for="item in tours[currentTourIndex].trips"
           :key="item.id"
           :lat-lngs="item.coordinates"
           :color="item.color"
+          :weight=6
           v-on:click="loadContent(item.id)"
           v-on:mouseover="changeColor(item.id)"
           v-on:mouseleave="changeColor(item.id)"/>
-        </div>
+          <l-marker
+            v-for="item in tours[currentTourIndex].trips"
+            :key="item.title"
+            :lat-lng="item.loc"
+            v-on:click="loadContent(item.id)"
+          >
+          <l-tooltip>
+            {{item.title}} <br>
+            Click for more info.
+            </l-tooltip>
+          </l-marker>
       </section>
 
-      <!-- card -->
+      <!-- trip info modal -->
       <b-modal
         id="bv-modal-trip"
         hide-footer
@@ -94,12 +142,12 @@
       >
         <template #modal-title>
           <div v-if="showTripDetails">
-            {{tripsForTour[currentTour].title}}
+            {{tours[currentTourIndex].trips[currentTrip].title}}
           </div>
         </template>
           <div v-if="showTripDetails">
           <!--TODO add images-->
-            {{tripsForTour[currentTour].text}}
+            {{tours[currentTourIndex].trips[currentTrip].text}}
         </div>
         <b-button class="mt-3" block @click="closeContent()">Close Me</b-button >
       </b-modal>
@@ -108,14 +156,19 @@
 </template>
 
 <script>
-import { latLng, latLngBounds } from "leaflet";
+import {
+    latLng,
+    latLngBounds
+} from "leaflet";
 import {
   LMap,
   LTileLayer,
+  LTooltip,
   LControl,
   LControlScale,
   LControlZoom,
   LPolyline,
+  LMarker,
 } from "vue2-leaflet";
 import { BButton } from "bootstrap-vue";
 
@@ -126,8 +179,10 @@ export default {
     LControl,
     LControlScale,
     LControlZoom,
-    BButton,
     LPolyline,
+    LMarker,
+    LTooltip,
+    BButton,
   },
   data() {
     return {
@@ -143,24 +198,18 @@ export default {
         zoomSnap: 0.5,
         zoomControl: false,
       },
-      loading: true,
-      failedToLoad: false,
+      loaded: false,
       tourHasTrips: false,
       showTripDetails: false,
       errorText: null,
       tourBounds: null,
-      currentTour: null,
+      currentTrip: null,
+      currentTourIndex: null,
       selectedTour: null,
-      tourDropdown: [
-        {
-          value: null,
-          text: "Select tour",
-        },
-      ],
-      tripsForTour:[],
+      tours: [],
       selectedTrip: null,
       tripDropdown: [],
-    };
+    }
   },
   methods: {
     zoomUpdate(zoom) {
@@ -188,99 +237,126 @@ export default {
         this.tours[this.currentTourIndex].trips[this.currentTrip].color = 'red';
         this.$bvModal.show("bv-modal-trip");
         let tripBounds = latLngBounds();
-        for(let coordinate in this.tripsForTour[itemId].coordinates){
-            tripBounds.extend(this.tripsForTour[itemId].coordinates[coordinate]);
+        for(let coordinate in this.tours[this.currentTourIndex].trips[itemId].coordinates){
+            tripBounds.extend(this.tours[this.currentTourIndex].trips[itemId].coordinates[coordinate]);
         }
         if(tripBounds.isValid())
             this.$refs.map.mapObject.fitBounds(tripBounds);
     },
     closeContent() {
         this.showTripDetails = false;
-        this.tripsForTour[this.currentTour].color = 'black';
+        this.tours[this.currentTourIndex].trips[this.currentTrip].color = "black";
         this.$bvModal.hide('bv-modal-trip');
         this.selectedTrip = null;
         if(this.tourBounds.isValid())
             this.$refs.map.mapObject.fitBounds(this.tourBounds);
+    },
+    drawBounds(index, tourId) {
+        if(this.tourBounds.isValid()){
+            this.$refs.map.mapObject.fitBounds(this.tourBounds);
+            this.tourHasTrips = true;
+            this.loaded = true;
+            this.currentTourIndex = index;
+        }else{
+            this.errorText = "There are no Coordinates available for Tour " + tourId + ".";
+            this.$bvModal.show("bv-modal-error");
+        }
     },
     retryConnect() {
         this.$bvModal.hide('bv-modal-error');
         this.getTours();
     },
     changeColor(itemId) {
-        if(this.tripsForTour[itemId].color == 'black')
+        if(this.tours[this.currentTourIndex].trips[itemId].color === "black")
         {
-            this.tripsForTour[itemId].color = 'red';
+            this.tours[this.currentTourIndex].trips[itemId].color = "red";
             this.selectedTrip = itemId;
         }
         else if(!this.showTripDetails)
         {
-            this.tripsForTour[itemId].color = 'black';
+            this.tours[this.currentTourIndex].trips[itemId].color = "black";
             this.selectedTrip = null;
         }
     },
     async getTours() {
         fetch(this.endpoint + "/tours",{ "method":"GET" })
-            .then((response) => {
-                if(response.status === 200){
-                       return response.json();
-                    }else{
-                        throw new Error("HTTP status " + response.status);
+        .then((response) => {
+            this.$bvModal.show("bv-modal-loading");
+            if(response.status === 200){
+                return response.json();
+            }else{
+                    throw new Error("HTTP status " + response.status);
                     }})
-            .then((data) =>
-            {
+        .then((data) =>
+                {
                 data.forEach((v) =>{
-                        this.tourDropdown.push({value : v.id, text: v.title});
+                        this.tours.push({value : v.id, text: v.title, trips: []});
                         });
-                if(this.tourDropdown.length > 1){
-                    this.selectedTour = 1;
-                    this.getCoordinates(this.selectedTour);
+                if(this.tours.length > 0){
+                this.selectedTour = this.tours[this.tours.length-1].value;
+                this.getCoordinates(this.tours.length);
+                }else{
+                this.errorText = "There are no Tours saved on the server.";
+                this.$bvModal.show("bv-modal-error");
                 }
-            })
-            .catch(err=>{
-                this.failedToLoad = true;
+                })
+        .catch(err=>{
+                this.$bvModal.hide("bv-modal-loading");
                 this.errorText = "Could not connect to the API. (" + err + ")";
                 this.$bvModal.show("bv-modal-error");
-                })
+                });
     },
-    async getCoordinates(tourId) {
-        fetch(this.endpoint + "/tours/" + tourId + "/trips",{"method":"GET" })
-            .then((response) => {
-                if(response.status === 200){
-                       return response.json();
-                    }else{
-                        throw new Error("HTTP status " + response.status);
-                    }})
-            .then((data) => {
-                this.tripsForTour = [];
-                var i = 0;
-                this.tripDropdown = [];
-                this.tripDropdown.push({value: null, text: "Blog entry"});
-                this.tourBounds = latLngBounds();
-                data.forEach((value) =>{
-                        var coordinates = [];
-                        value.coordinates.forEach((v) => {
-                                coordinates.push([v.lat,v.lng]);
-                                this.tourBounds.extend([v.lat,v.lng]);
-                                });
-                        this.tripsForTour.push({id: i, title: value.title, text: value.text, coordinates: coordinates, color: 'black'});
-                        this.tripDropdown.push({value : i, text: value.title});
-                        i++;
-                        });
 
-                if(this.tourBounds.isValid())
-                    this.$refs.map.mapObject.fitBounds(this.tourBounds);
-                this.tourHasTrips = true;
+    async getCoordinates(tourId) {
+        let index = this.tours.map(function(e) { return e.value; }).indexOf(tourId);
+        this.tourBounds = latLngBounds();
+        if(this.tours[index].trips.length > 0){
+            for(let trip in this.tours[index].trips){
+                for(let coordinate in this.tours[index].trips[trip].coordinates){
+                    this.tourBounds.extend(this.tours[index].trips[trip].coordinates[coordinate]);
+                }
+            }
+                    this.drawBounds(index, tourId);
+        }else{
+            fetch(this.endpoint + "/tours/" + tourId + "/trips",{"method":"GET" })
+                .then((response) => {
+                        this.$bvModal.show("bv-modal-loading");
+                        if(response.status === 200){
+                        return response.json();
+                        }else{
+                        throw new Error("HTTP status " + response.status);
+                        }
+                        })
+            .then((data) => {
+                    let i = 0;
+                    //this.tours[index].trips.push({id: null, title: "Select a stage"});
+                    data.forEach((value) =>{
+                            var coordinates = [];
+                            value.coordinates.forEach((v) => {
+                                    coordinates.push([v.lat,v.lng]);
+                                    this.tourBounds.extend([v.lat,v.lng]);
+                                    });
+                            //TODO id should be value.id
+
+                            this.tours[index].trips.push({id: i, title: value.title, text: value.text, coordinates: coordinates, color: "black", loc: coordinates[parseInt(coordinates.length/2)]});
+                            i++;
+                            });
+                    this.drawBounds(index, tourId);
             })
             .catch(err=>{
                     this.errorText = "Could not connect to the API. (" + err + ")";
                     this.$bvModal.show("bv-modal-error");
+                    })
+            .finally(() => {
+                this.$bvModal.hide("bv-modal-loading");
             });
-            }
-  },
-  async beforeMount() {
+        }
+        },
+    },
+    async beforeMount() {
         this.getTours();
-  },
-};
+    },
+}
 </script>
 
 <style scoped>

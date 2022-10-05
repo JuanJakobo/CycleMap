cc31ba2e149cc20909396a3f6f8ad2ae863a49f0
JuanJakobo <github@johannsn.com>
2022-10-05 18:36:30 +0200

    Add CORS endpoint from application.properties

--- backend/src/main/kotlin/com/johannsn/cyclemapbackend/CyclemapBackendApplication.kt
+++ backend/src/main/kotlin/com/johannsn/cyclemapbackend/CyclemapBackendApplication.kt
@@ -1,7 +1,12 @@
 package com.johannsn.cyclemapbackend
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class CyclemapBackendApplication
@@ -10,4 +15,15 @@ fun main(args: Array<String>) {
 	runApplication<CyclemapBackendApplication>(*args)
 }
 
-
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer
+ {
+	 @Value("\${CycleMap.allowedEndpoint}")
+	 private lateinit var allowedEndpoint: String
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+			.allowedMethods("GET")
+			.allowedOrigins(allowedEndpoint)
+	}
+}

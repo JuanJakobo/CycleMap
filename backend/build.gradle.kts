3b18cd800dfe1d4ac9bee3368f3251cde4d08787
JuanJakobo <github@johannsn.com>
2022-10-09 10:17:20 +0200

    switch to mysql

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,9 +22,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-security:2.7.3")
+	implementation("org.springframework.boot:spring-boot-starter-security:2.7.4")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
+	runtimeOnly("mysql:mysql-connector-java:8.0.30")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
